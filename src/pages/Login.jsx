0c49dc17e5912586505517f673d8a8aa71d2f94c
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-08-22 16:38:24 +0200

    updated login and register components and add new features styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,20 +3,20 @@ import { Form, Link } from 'react-router-dom';
 
 export const Login = () => {
   return (
-    <section className="h-screen grid place-items-center">
+    <section className="h-screen grid place-items-center ">
       <Form method="post" className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
-        <h4 className="text-center text-3xl font-bold">Login</h4>
-        <FormInput type="email" name="identifier" defaultValue="test@test.com" />
-        <FormInput type="password" label="password" name="password" />
-        <div className="mt-4">
-          <SubmitBtn text="login" />
+        <h4 className="text-center text-3xl font-bold font-openSans uppercase">Login</h4>
+        <FormInput type="email" label="Email" name="identifier" defaultValue="test@test.com" />
+        <FormInput type="password" label="Password" name="password" />
+        <div className="mt-4 capitalize">
+          <SubmitBtn text="Sign in" />
         </div>
-        <button type="button" className="btn btn-secondary btn-block">
+        <button type="button" className="btn btn-secondary btn-block capitalize ">
           guest user
         </button>
         <p className="text-center">
           Not a member yet?{' '}
-          <Link to="/register" className="ml-2 link link-hover link-primary capitalize">
+          <Link to="/register" className="ml-2 link link-hover link-primary uppercase">
             register
           </Link>
         </p>
